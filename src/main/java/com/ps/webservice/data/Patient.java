8a97be153c8898888d4a7866a0c3ed4a55ac1b68
waldo <waldo@waldo-MS-7A34>
2018-11-17 16:16:21 -0500

    One class, called patient.

---
+++ src/main/java/com/ps/webservice/data/Patient.java
@@ -0,0 +1,78 @@
+package com.ps.webservice.data;
+
+import java.util.List;
+
+
+
+public class Patient  {
+	
+	private String Id;
+	private String firstName;
+	private String lastName;
+	private String DateOfBirth;
+	private int age;
+	private String HealthInsurance;
+	private String userName;
+	private String password;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getDateOfBirth() {
+		return DateOfBirth;
+	}
+
+
+	public void setDateOfBirth(String dateOfBirth) {
+		DateOfBirth = dateOfBirth;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+
+	public String getHealthInsurance() {
+		return HealthInsurance;
+	}
+
+
+	public void setHealthInsurance(String healthInsurance) {
+		HealthInsurance = healthInsurance;
+	}
+
+
+	
+	
+
+	public void login(String userName, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
