bc204f0c1f23e92f28b6ed6cecca55e33763ac3c
waldo <waldo@waldo-MS-7A34>
2018-11-17 16:58:05 -0500

    sdafv

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -1,9 +1,16 @@
 package com.aws.codestar.projecttemplates.controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.ps.webservice.data.Patient;
+import com.ps.webservice.data.mapper.PatientDataMapper;
 
 /**
  * Basic Spring MVC controller that handles all GET requests.
@@ -29,5 +36,21 @@ public class HelloWorldController {
 	 public String helloWorld2() {
 	 	  return  "Hello World";
 	 }
+	 
+	 PatientDataMapper dp = new PatientDataMapper();
+		@RequestMapping(value="/users",method = RequestMethod.GET)
+		    public List<Patient> allPatient() {
+		     List<Patient> patients = null;
+			try {
+				patients = dp.mappedPatientInformation();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		     if(patients ==null || patients.isEmpty()) {
+		    	 System.out.println(HttpStatus.BAD_REQUEST + "Hit Endpoint"); ;
+		     }
+			 return patients;
+		    }
 
 }
