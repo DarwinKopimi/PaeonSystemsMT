f5484da6c8a9cbd47a5475391e299ea9a4cfdeb4
waldo <waldo@waldo-MS-7A34>
2018-11-18 16:11:37 -0500

    daata

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -1,9 +1,17 @@
 package com.aws.codestar.projecttemplates.controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.ps.webservice.data.Patient;
+import com.ps.webservice.data.mapper.PatientDataMapper;
 
 /**
  * Basic Spring MVC controller that handles all GET requests.
@@ -30,22 +38,22 @@ public class HelloWorldController {
 	 	  return  "Hello World";
 	 }
 	 
-//	 PatientDataMapper dp = new PatientDataMapper();
+	 PatientDataMapper dp = new PatientDataMapper();
 	 
-//		@SuppressWarnings("unchecked")
-//		@RequestMapping(value="/users",method = RequestMethod.GET)
-//		    public List<Patient> allPatient() {
-//		     List<Patient> patients = null;
-//			try {
-//				patients = dp.mappedPatientInformation();
-//			} catch (SQLException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//		     if(patients ==null || patients.isEmpty()) {
-//		    	 System.out.println(HttpStatus.BAD_REQUEST + "Hit Endpoint"); ;
-//		     }
-//			 return (List<Patient>) new ResponseEntity (patients, HttpStatus.OK);
-//		    }
+		@SuppressWarnings("unchecked")
+		@RequestMapping(value="/users",method = RequestMethod.GET)
+		    public List<Patient> allPatient() {
+		     List<Patient> patients = null;
+			try {
+				patients = dp.mappedPatientInformation();
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+		     if(patients ==null || patients.isEmpty()) {
+		    	 System.out.println(HttpStatus.BAD_REQUEST + "Hit Endpoint"); ;
+		     }
+			 return (List<Patient>) new ResponseEntity (patients, HttpStatus.OK);
+		    }
 
 }
